300c995ab394e8338f45b9a35e7e23e36b6fdfac
evgluzedev <evgeniy.shostak.04@mail.ru>
2024-08-19 21:13:31 +0300

    Initial commit

---
+++ src/main/java/com/optimagrowth/licensing_service/LicensingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.licensing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicensingServiceApplication.class, args);
+	}
+
+}
